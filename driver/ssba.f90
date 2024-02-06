32b91ac6c1aca60b6d37228eb13a87187fc6e2cb
clstaelen <159060836+clstaelen@users.noreply.github.com>
2024-02-06 12:12:47 +0100

    Update ssba.f90

--- driver/ssba.f90
+++ driver/ssba.f90
@@ -1,30 +1,30 @@
 Program ssba
   ! Staelen & Huré, A&A, 2024 "Approaching the structure of rotating bodies from dimension reduction"
   ! compile and run with
-  ! gfortran -O3 -Wall -unused ssba.f90; ./a.out
+  ! gfortran -Wall -unused ssba.f90; ./a.out
   Implicit None
   Integer,Parameter::AP=Kind(1.00D+00)
   Real(kind=AP),Parameter::pi=Atan(1._AP)*4,threshold=1.E-14_AP
   Integer::i,j,iter,n,itermax
-  Real(kind=AP)::error,ebar_surf,npoly,int1,int2,C1old,mass,inemom,angmom,Om2
-  Real(kind=AP),Allocatable::ebar(:),ell2(:),a(:),c(:,:),rho(:),hent(:),dhent(:),de2da(:),kernel(:),omega2(:)
+  Real(kind=AP)::error,ebar_surf,npoly,int1,int2,mass,inemom,angmom,Om2
+  Real(kind=AP),Allocatable::ebar(:),ell2(:),a(:),rho(:),hent(:),drho(:),debar(:),de2da(:),kernel(:),omega2(:)
   Logical::notfinished
   ! statements
   ! input
   n=2**8
-  Allocate(ebar(0:n),ell2(0:n),a(0:n),c(0:n,0:n),rho(0:n),hent(0:n),dhent(0:n),de2da(0:n),kernel(0:n),omega2(0:n))
+  Allocate(ebar(0:n),ell2(0:n),a(0:n),rho(0:n),hent(0:n),drho(0:n),debar(0:n),de2da(0:n),kernel(0:n),omega2(0:n))
   itermax=100
-  ! initialisation and starting guess (linear profiles)
+  ! initialisation and starting guess (quadratic profiles)
   ebar_surf=0.9_AP
   npoly=3._AP
   print '("SSB-approximation (Staelen & Huré, A&A, 2024) - see Tab. 1")'
   print '("Nodes, surface axis ratio, poly. index",1X,i4,2(1X,1PE13.6))',n,ebar_surf,npoly
   a(:)=1._AP*(/(i,i=0,n)/)/n
   ebar(n)=ebar_surf
-  ebar(0:n-1)=1._AP-(1._AP-ebar_surf)*a(0:n-1)
+  ebar(0:n-1)=1._AP-(1._AP-ebar_surf)*a(0:n-1)**2
   ell2(:)=1._AP-ebar(:)**2
   hent(0)=1._AP
-  hent(1)=0._AP
+  hent(n)=0._AP
   hent(1:n-1)=1._AP-a(1:n-1)**2
   rho(0)=1._AP
   rho(n)=0._AP
@@ -36,7 +36,8 @@ Program ssba
   ! SCF-loop
   Do While (notfinished)
      iter=iter+1
-     C1old=hent(0)
+     debar(0:n) = ebar(0:n)
+     drho(0:n)=rho(0:n)
      ! computes de2/da (a>0)
      Do i=1,n
         kernel(0:n)=(/(mu(j,i),j=0,n)/)
@@ -50,21 +51,17 @@ Program ssba
         Call trapezoidal(n-i+1,a(i:n),de2da(i:n),int1)
         ell2(i)=1._AP-ebar_surf**2-int1
      End Do
-     ebar(:)=Sqrt(1._AP-ell2(:))
-     ! confocal parameters
-     Do i=0,n
-        c(0:n,i)=(/(a(j)**2*ell2(j)/a(i)**2-ell2(i),j=0,n)/)
-     End Do
+     ebar(0:n)=Sqrt(1._AP-ell2(0:n))
+     debar(0:n) = abs(debar(0:n)-ebar(0:n))
      ! computes the enthalpy H(a)
-     dhent=hent
      Do i=0,n
         kernel(0:n)=(/(eta(j,n)-eta(j,i),j=0,n)/)*pi*2
         Call trapezoidal(n+1,rho(0:n),kernel(0:n),hent(i))
      End Do
      ! computes the mass density rho(a)
      rho(1:n-1)=(hent(1:n-1)/hent(0))**npoly
-     dhent=dhent-hent
-     error=Sum(Abs(dhent(0:n)))/(n+1)
+     drho(0:n)=abs(drho(0:n)-rho(0:n))
+     error=max(maxval(debar),maxval(drho))
      ! Print '(i4,1X,1PE13.6)',iter,error
      If ((error<threshold).Or.(iter>itermax)) notfinished=.False.
   End Do
@@ -93,11 +90,11 @@ Program ssba
   Print '("Mass and mean rotation rate J/I squared",2(1X,1PE13.6))',mass,Om2
   Open(unit=1,file="ssba.dat")
   Do i=0,n 
-     Write(1,'(i4,6(1X,1PE13.6))') i,a(i),de2da(i),ell2(i),rho(i),omega2(i),dhent(i)
+     Write(1,'(i4,6(1X,1PE13.6))') i,a(i),de2da(i),ell2(i),rho(i),omega2(i),drho(i)
   End Do
   Print '("End. See output file ssba.dat")'
   Close(1)
-  Deallocate(ebar,ell2,a,c,rho,hent,dhent,de2da,kernel,omega2)
+  Deallocate(ebar,ell2,a,rho,hent,drho,debar,de2da,kernel,omega2)
 
 Contains
 
@@ -121,62 +118,70 @@ Contains
     If (x>1._AP) ML_A=Log(x+Sqrt(x**2-1._AP))/Sqrt(x**2-1._AP)
   End Function ML_A
 
-  Function kappa(j,i)
-    Implicit None
-    Real(kind=AP)::kappa
-    Integer :: i,j
-    If (j<i) Then
-       kappa=a(j)/a(i)/ebar(i)*4/3*(1._AP-ebar(i))
-       If (ebar(j)/=1._AP)  kappa=a(j)*ebar(j)/a(i)/ell2(j)*((1._AP-a(j)**2*ell2(j)/a(i)**2*2)&
-            &*ML_A(Sqrt(1._AP-a(j)**2*ell2(j)/a(i)**2))&
-            &+Sqrt(1.+c(j,i))*ML_A(Sqrt(1._AP-a(j)**2*ell2(j)/a(i)**2/(1.+c(j,i))))*2&
-            &-ebar(i)*2-Sqrt(1._AP-a(j)**2*ell2(j)/a(i)**2))
-    Else
-       kappa=1._AP-(3._AP-ell2(i)*2)/3 
-       If (ebar(j)/=1._AP) kappa=1._AP+(3._AP-ell2(i)*2)/ell2(j)*(ebar(j)*ML_A(ebar(j))-1._AP)
-    End If
-  End Function kappa
+  function kappa(j,i)
+    implicit none ; real(kind=AP)::kappa,cji ; integer :: i,j
+    if (j<i) then
+      kappa = a(j)/a(i)/ebar(i)*4/3*(1._AP-ebar(i))
+      if (ebar(j)/=1._AP) then
+        cji = a(j)**2*ell2(j)/a(i)**2-ell2(i)
+        kappa = a(j)*ebar(j)/a(i)/ell2(j)*((1._AP-a(j)**2*ell2(j)/a(i)**2*2)&
+          &*ML_A(sqrt(1._AP-a(j)**2*ell2(j)/a(i)**2))&
+          &+sqrt(1.+cji)*ML_A(sqrt(1._AP-a(j)**2*ell2(j)/a(i)**2/(1.+cji)))*2&
+          &-ebar(i)*2-sqrt(1._AP-a(j)**2*ell2(j)/a(i)**2))
+      end if
+    else
+      kappa = 1._AP-(3._AP-ell2(i)*2)/3 
+      if (ebar(j)/=1._AP) then
+        kappa = 1._AP+(3._AP-ell2(i)*2)/ell2(j)*(ebar(j)*ML_A(ebar(j))-1._AP)
+      end if
+    end if
+  end function
 
-  Function chi(j,i)
-    Implicit None
-    Real(kind=AP)::chi
-    Integer :: i,j 
-    chi=0._AP
-    If (j<i) Then
-       chi=a(j)**3/a(i)**4*(ebar(i)-1._AP)/ebar(i)
-       If (ebar(j)/=1._AP) chi=a(j)**3*ebar(j)/a(i)**4*(ML_A(Sqrt(1._AP-a(j)**2*ell2(j)/a(i)**2))&
-            &-ML_A(Sqrt(1._AP-a(j)**2*ell2(j)/a(i)**2/(1._AP+c(j,i))))/Sqrt(1._AP+c(j,i)))
-    End If
-  End Function chi
+  function chi(j,i)
+    implicit none ; real(kind=AP)::chi,cji ; integer :: i,j
+    chi = 0._AP
+    if (j<i) then
+      chi = a(j)**3/a(i)**4*(ebar(i)-1._AP)/ebar(i)
+      if (ebar(j)/=1._AP) then
+        cji = a(j)**2*ell2(j)/a(i)**2-ell2(i)
+        chi = a(j)**3*ebar(j)/a(i)**4*(ML_A(sqrt(1._AP-a(j)**2*ell2(j)/a(i)**2))&
+        &-ML_A(sqrt(1._AP-a(j)**2*ell2(j)/a(i)**2/(1._AP+cji)))/sqrt(1._AP+cji))
+      end if
+    end if
+  end function
 
-  Function mu(j,i)
-    Implicit None
-    Real(kind=AP)::mu
-    Integer :: i,j
-    If (j<i) Then 
-       mu=-a(j)**3/a(i)**3/3/ebar(i)**3
-       If (ebar(j)/=1._AP) mu=a(j)*ebar(j)/a(i)/ell2(j)&
-            &*(ML_A(Sqrt(1.-a(j)**2*ell2(j)/a(i)**2/(1._AP+c(j,i))))/Sqrt(1._AP+c(j,i))-1._AP/ebar(i))
-    Else
-       mu=-1._AP/3
-       If (ebar(j)/=1._AP) mu=(ebar(j)*ML_A(ebar(j))-1._AP)/ell2(j)
-    End If
-  End Function mu
+  function mu(j,i)
+    implicit none ; real(kind=AP)::mu,cji ; integer :: i,j
+    if (j<i) then 
+      mu = -a(j)**3/a(i)**3/3/ebar(i)**3
+      if (ebar(j)/=1._AP) then
+        cji = a(j)**2*ell2(j)/a(i)**2-ell2(i)
+        mu = a(j)*ebar(j)/a(i)/ell2(j)*&
+          &(ML_A(sqrt(1.-a(j)**2*ell2(j)/a(i)**2/(1._AP+cji)))/sqrt(1._AP+cji)-1._AP/ebar(i))
+    end if
+    else
+      mu = -1._AP/3
+      if (ebar(j)/=1._AP) then
+        mu = (ebar(j)*ML_A(ebar(j))-1._AP)/ell2(j)
+    end if
+    end if
+  end function
 
-  Function eta(j,i)
-    Implicit None
-    Real(kind=AP)::eta
-    Integer :: i,j 
-    If (j<i) Then
-       eta=a(j)**3/a(i)/ebar(i)*2/3
-       If (ebar(j)/=1._AP) eta=-a(j)*ebar(j)/ell2(j)*(a(i)*ebar(i)&
-            &-Sqrt(a(i)**2*ebar(i)**2+a(j)**2*ell2(j))&
-            &*ML_A(Sqrt(1._AP-a(j)**2*ell2(j)/(a(i)**2*ebar(i)**2+a(j)**2*ell2(j)))))
-    Else
-       eta=a(j)**2-a(i)**2*ebar(i)**2/3
-       If (ebar(j)/=1._AP) eta = (a(i)**2*ebar(i)**2+a(j)**2*ell2(j))*ebar(j)/ell2(j)&
-            &*ML_A(ebar(j))-a(i)**2*ebar(i)**2/ell2(j)
-    End If
-  End Function eta
-
+  function eta(j,i)
+    implicit none ; real(kind=AP)::eta ; integer :: i,j 
+    if (j<i) then
+      eta = a(j)**3/a(i)/ebar(i)*2/3
+      if (ebar(j)/=1._AP) then
+      eta=-a(j)*ebar(j)/ell2(j)*(a(i)*ebar(i)&
+        &-sqrt(a(i)**2*ebar(i)**2+a(j)**2*ell2(j))&
+        &*ML_A(sqrt(1._AP-a(j)**2*ell2(j)/(a(i)**2*ebar(i)**2+a(j)**2*ell2(j)))))
+      end if
+    else
+      eta = a(j)**2-a(i)**2*ebar(i)**2/3
+      if (ebar(j)/=1._AP) then
+        eta = (a(i)**2*ebar(i)**2+a(j)**2*ell2(j))*ebar(j)/ell2(j)&
+          &*ML_A(ebar(j))-a(i)**2*ebar(i)**2/ell2(j)
+      end if
+    end if
+  end function
 End Program ssba
